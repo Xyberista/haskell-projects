1f1dfad97b95df6bd70fe4b81930782fc1c66075
Xyberista <50119873+Xyberista@users.noreply.github.com>
2021-05-31 14:49:05 -0500

    set up empty board as a Map of Maps. added a make move function

--- src/TicTacToe.hs
+++ src/TicTacToe.hs
@@ -1,6 +1,20 @@
 module TicTacToe where
 
-emptyBoard :: [[String]]
-emptyBoard = [["", "", ""]
-             ,["", "", ""]
-             ,["", "", ""]]
+import qualified Data.Map as Map
+import Data.Maybe
+
+data Move = Move { col :: Int
+                 , row :: Int
+                 , symbol :: String}
+            
+newtype GameState = GameState Board
+
+type Board = Map.Map Int Row
+type Row = Map.Map Int String
+
+emptyBoard :: Board
+emptyBoard = Map.fromList $ map (\y -> (y, Map.fromList $ map (\x -> (x, "")) [0..2])) [0..2]
+
+makeMove :: Board -> Move -> Board
+makeMove board (Move x y sym) =
+  Map.alter (\r -> Just $ Map.alter (\_ -> Just sym) x $ fromJust r) y board
