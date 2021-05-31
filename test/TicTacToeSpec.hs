073ac171f18a5f087f4748ec45a35a7bd669a263
Xyberista <50119873+Xyberista@users.noreply.github.com>
2021-05-31 18:00:28 -0500

    added tests for the displaying of the board

--- test/TicTacToeSpec.hs
+++ test/TicTacToeSpec.hs
@@ -3,6 +3,7 @@ module TicTacToeSpec (spec) where
 import Test.Hspec
 import Test.QuickCheck
 
+import Control.Monad.IO.Class
 import qualified Data.Map as Map
 import Data.Maybe
 
@@ -16,13 +17,42 @@ spec = do
       emptyBoard `shouldBe` Map.fromList [(0,r), (1,r), (2,r)]
 
   describe "moves" $ do
-    it "makes move when target slot is empty" $ do
-      let board = emptyBoard
-      let (possible, newBoard) = makeMove board (Move 0 0 "x")
-      possible `shouldBe` True
-      fromJust (Map.lookup 0 $ fromJust $ Map.lookup 0 newBoard) `shouldBe` "x"
+    context "when the target slot is empty" $ do
+      it "move is successfully made" $ do
+        let board = emptyBoard
+        let (possible, newBoard) = makeMove board (Move 0 0 "x")
+        possible `shouldBe` True
+        fromJust (Map.lookup 0 $ fromJust $ Map.lookup 0 newBoard) `shouldBe` "x"
+  
+    context "when the target slot is not empty" $ do
+      it "move is unsuccessful, board is unmodified" $ do
+        let board = emptyBoard
+        let (_, temp) = makeMove board (Move 0 0 "x")
+        let (possible, newBoard) = makeMove temp (Move 0 0 "x")
+        possible `shouldBe` False
+        newBoard `shouldBe` temp
         
-        
-        
-        
+    context "when the target slot is out of bounds" $ do
+      it "move is unsuccessful, board is unmodified" $ do
+        let board = emptyBoard
+        let (possible, newBoard) = makeMove board (Move 4 4 "x")
+        possible `shouldBe` False
+        board `shouldBe` newBoard
+
+  describe "display board" $ do
+    context "when empty board" $ do
+      it "prints empty board" $ do
+        let expected = " | | \n-----\n | | \n-----\n | | \n"
+        let game = GameState emptyBoard
+        let actual = gameToString game
+        actual `shouldBe` expected
+        (displayBoard game) >>= \x -> putStrLn expected >>= (`shouldBe` x)
+  
+    context "when non-empty board" $ do
+      it "prints board" $ do
+        let expected = "x| | \n-----\n | | \n-----\n | | \n"
+        let (_, game) = makeMove emptyBoard (Move 0 0 "x")
+        let actual = gameToString $ GameState game
+        actual `shouldBe` expected
+        (displayBoard (GameState game)) >>= \x -> putStrLn expected >>= (`shouldBe` x)
         
